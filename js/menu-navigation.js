e5e8381a057fe3b8b812deec3e4fefc94af6e2fb
Rodrigo Carvalho <125412121+rodrigoCarvalho13@users.noreply.github.com>
2023-05-19 19:20:36 -0300

    Delete js directory

--- js/menu-navigation.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var topNav = document.getElementById("servicos");
-    if (topNav.className === "servicos") {
-      topNav.className += " responsive";
-    } else {
-      topNav.className = "header";
-    }
-  }
